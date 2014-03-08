aa4d8e82777da5a02b00ccbaf06d635faff71de1
Mihail Shumilov <mschumilow@gmail.com>
2014-03-08 23:36:56 +0200

    finish first version of news links detector

--- application/protected/components/NewsParser.php
+++ application/protected/components/NewsParser.php
@@ -6,15 +6,116 @@
  * Date: 07.03.14
  * Time: 13:41
  */
-class NewsParser extends CApplicationComponents
+class NewsParser extends CApplicationComponent
 {
-    public function __constructor(Source $source)
-    {
+    private $url;
+    private $categoryPattern;
+    private $newsPattern;
+    private $sourceId;
 
+    private $newsLinks = array();
+    private $categoryLinks = array();
+
+    public function __construct(Source $source)
+    {
+        $this->url             = $source->url;
+        $this->categoryPattern = $source->category_pattern;
+        $this->newsPattern     = $source->news_pattern;
+        $this->sourceId        = $source->id;
     }
 
     public function run()
     {
+        $firstPage = $this->load($this->url);
+        $this->detectNewsLinks($firstPage);
+        $this->detectCategoryLinks($firstPage);
+        $this->processCategoryLinks();
+        $this->fillParserQueue();
+    }
 
+    protected function load($url)
+    {
+        if (filter_var($url, FILTER_VALIDATE_URL)) {
+            try {
+                return file_get_contents($url);
+            } catch (Exception $e) {
+
+            }
+        } else {
+            throw new Exception("No valid url: `{$url}`");
+        }
+    }
+
+    private function detectCategoryLinks($content)
+    {
+        if ($content) {
+            $this->categoryLinks = $this->linkDetector($content, $this->categoryPattern);
+        } else {
+            throw new Exception("No content data");
+        }
+    }
+
+    private function detectNewsLinks($content)
+    {
+        if ($content) {
+            $this->newsLinks = $this->linkDetector($content, $this->newsPattern);
+        } else {
+            throw new Exception("No content data");
+        }
+    }
+
+    private function linkDetector($content, $pattern)
+    {
+        if ($content && $pattern) {
+            $returnLink = array();
+            preg_match_all('/href=\"([^\"]*)\"/i', $content, $results);
+            if ($results[1]) {
+                foreach ($results[1] as $link) {
+                    $link = str_replace($this->url, '', $link);
+                    if (preg_match("/^{$pattern}$/i", $link)) {
+                        $returnLink[] = $link;
+                    }
+                }
+                return $returnLink;
+            }
+        } else {
+            throw new Exception("No content or pattern data");
+        }
+    }
+
+    private function processCategoryLinks()
+    {
+        if (is_array($this->categoryLinks)) {
+            $cnt = 0;
+            foreach ($this->categoryLinks as $url) {
+                $cnt++;
+                if ($content = $this->load($this->url . $url)) {
+                    $this->detectNewsLinks($content);
+                }
+                $percentage = round($cnt / sizeof($this->categoryLinks) * 100, 2);
+                echo "Completed:`{$percentage}%`\r";
+            }
+        }
+    }
+
+    private function fillParserQueue()
+    {
+        $this->newsLinks = array_unique($this->newsLinks);
+        foreach ($this->newsLinks as $link) {
+            try {
+                $pqItem             = new ParserQueue();
+                $pqItem->source_id  = $this->sourceId;
+                $pqItem->url        = $link;
+                $pqItem->status     = ParserQueue::STATUS_NEW;
+                $pqItem->created_at = new CDbExpression('NOW()');
+                if ($pqItem->save()) {
+
+                } else {
+                    print_r($pqItem->getErrors());
+                }
+            } catch (CDbException $e) {
+
+            }
+        }
     }
 } 
--- application/protected/models/ParserQueue.php
+++ application/protected/models/ParserQueue.php
@@ -4,8 +4,37 @@ Yii::import('application.models._base.BaseParserQueue');
 
 class ParserQueue extends BaseParserQueue
 {
+
+    const  STATUS_NEW = 'new';
+
     public static function model($className = __CLASS__)
     {
         return parent::model($className);
+    }
+
+    public function rules()
+    {
+        return array(
+            array('source_id, url, created_at', 'required'),
+            array('source_id', 'numerical', 'integerOnly' => true),
+            array('status', 'length', 'max' => 11),
+            array('updated_at', 'safe'),
+            array('status', 'default', 'setOnEmpty' => true, 'value' => null),
+            array('id, source_id, url, status, created_at, updated_at', 'safe', 'on' => 'search'),
+            array(
+                'updated_at',
+                'default',
+                'value'      => new CDbExpression('NOW()'),
+                'setOnEmpty' => true,
+                'on'         => 'update'
+            ),
+            array(
+                'created_at,updated_at',
+                'default',
+                'value'      => new CDbExpression('NOW()'),
+                'setOnEmpty' => true,
+                'on'         => 'insert'
+            )
+        );
     }
 }
