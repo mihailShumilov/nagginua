70a26555542acb22e2a2445ff36cba11909bb924
Mihail Shumilov <mschumilow@gmail.com>
2014-04-04 14:00:40 +0300

    added auto categorization

---
+++ application/protected/models/Categories.php
@@ -0,0 +1,11 @@
+<?php
+
+Yii::import('application.models._base.BaseCategories');
+
+class Categories extends BaseCategories
+{
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+}
