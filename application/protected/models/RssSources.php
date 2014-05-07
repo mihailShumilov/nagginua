776f9e833fb4ee3f08b2e94b1662e6f929f8b59b
Mihail Shumilov <mschumilow@gmail.com>
2014-05-07 17:16:14 +0300

    init rss sources model

---
+++ application/protected/models/RssSources.php
@@ -0,0 +1,11 @@
+<?php
+
+Yii::import('application.models._base.BaseRssSources');
+
+class RssSources extends BaseRssSources
+{
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+}
