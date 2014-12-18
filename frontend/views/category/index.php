ffaba005c6c603278122e1f0f31b21ec4b8749c4
Mihail Shumilov <mschumilow@gmail.com>
2014-12-18 23:03:48 +0200

    finish category page

--- frontend/views/category/index.php
+++ frontend/views/category/index.php
@@ -3,57 +3,55 @@
     <div class="outertight m-t-no">
         <?php $item = array_shift( $news ); ?>
         <div class="badg">
-            <p><a href="#">Featured.</a></p>
+            <p><a href="<?= $item->getLink(); ?>">Свежая.</a></p>
         </div>
         <div class="flexslider">
             <ul class="slides">
                 <li>
-                    <img src="img/trash/25.png" alt="MyPassion"/>
-                </li>
-                <li>
-                    <img src="img/trash/24.png" alt="MyPassion"/>
-                </li>
-                <li>
-                    <img src="img/trash/26.png" alt="MyPassion"/>
+                    <img src="<?= $item->getThumbLink( 'thumbCategory' ); ?>"
+                         alt="<?= \yii\helpers\Html::encode( $item->title ); ?>"/>
                 </li>
             </ul>
         </div>
 
-        <h6 class="regular"><a href="single.html">Blandit Rutrum, Erat et Sagittis. Lorem
-                Ipsum Dolor, Sit Amet Adipsing.</a></h6>
-        <span class="meta">26 May, 2013.   \\   <a href="#">World News.</a>   \\   <a href="#">No Coments.</a></span>
+        <h6 class="regular"><a href="<?= $item->getLink(); ?>"><?= \yii\helpers\Html::encode( $item->title ); ?></a>
+        </h6>
+        <span class="meta"><?= date( "d M, Y", strtotime( $item->created_at ) ); ?>
+            <?php if ($categories = $item->getCategoryList()): ?>
+                <?php foreach ($categories as $category): ?>
+                    \\ <a href="<?= $category->getLink(); ?>"><?= $category->name; ?></a>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </span>
 
-        <p>Blandit rutrum, erat et egestas ultricies, dolor tortor egestas enim, quiste rhoncus sem purus eu sapien.
-            Curabitur a orci nec risus lacinia vehic. Lorem ipsum
-            dolor adipcising elit. Erat egestan sagittis lorem aupo dolor sit ameta, auctor libero tempor...</p>
+        <p><?= \yii\helpers\Html::encode( $item->getShort() ); ?></p>
     </div>
 
     <div class="outertight m-r-no m-t-no">
         <?php $item = array_shift( $news ); ?>
         <div class="badg">
-            <p><a href="#">Featured.</a></p>
+            <p><a href="<?= $item->getLink(); ?>">Свежая.</a></p>
         </div>
         <div class="flexslider">
             <ul class="slides">
                 <li>
-                    <img src="img/trash/27.png" alt="MyPassion"/>
-                </li>
-                <li>
-                    <img src="img/trash/26.png" alt="MyPassion"/>
-                </li>
-                <li>
-                    <img src="img/trash/24.png" alt="MyPassion"/>
+                    <img src="<?= $item->getThumbLink( 'thumbCategory' ); ?>"
+                         alt="<?= \yii\helpers\Html::encode( $item->title ); ?>"/>
                 </li>
             </ul>
         </div>
 
-        <h6 class="regular"><a href="single.html">Blandit Rutrum, Erat et Sagittis. Lorem
-                Ipsum Dolor, Sit Amet Adipsing.</a></h6>
-        <span class="meta">26 May, 2013.   \\   <a href="#">World News.</a>   \\   <a href="#">No Coments.</a></span>
+        <h6 class="regular"><a href="<?= $item->getLink(); ?>"><?= \yii\helpers\Html::encode( $item->title ); ?></a>
+        </h6>
+        <span class="meta"><?= date( "d M, Y", strtotime( $item->created_at ) ); ?>
+            <?php if ($categories = $item->getCategoryList()): ?>
+                <?php foreach ($categories as $category): ?>
+                    \\ <a href="<?= $category->getLink(); ?>"><?= $category->name; ?></a>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </span>
 
-        <p>Blandit rutrum, erat et egestas ultricies, dolor tortor egestas enim, quiste rhoncus sem purus eu sapien.
-            Curabitur a orci nec risus lacinia vehic. Lorem ipsum
-            dolor adipcising elit. Erat egestan sagittis lorem aupo dolor sit ameta, auctor libero tempor...</p>
+        <p><?= \yii\helpers\Html::encode( $item->getShort() ); ?></p>
     </div>
 
     <div class="outerwide">
@@ -73,7 +71,9 @@
                 'class' => false
             ],
             'prevPageCssClass' => 'first-page',
-            'nextPageCssClass' => 'last-page'
+            'nextPageCssClass' => 'last-page',
+            'prevPageLabel'    => '&nbsp;',
+            'nextPageLabel'    => '&nbsp;'
         ] );
         ?>
     </div>
