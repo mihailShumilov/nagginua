1f1a80b2c82419553f6b1bf2721789dc501d2578
Mihail Shumilov <mschumilow@gmail.com>
2015-03-21 08:33:48 +0200

    change compression values

--- frontend/views/layouts/_parts/sidebar.php
+++ frontend/views/layouts/_parts/sidebar.php
@@ -77,28 +77,5 @@
         <div id="minfincomua_i_rub">Загружаем межбанк от <a href="http://minfin.com.ua/">minfin.com.ua</a></div>
         <script type="text/javascript" src="http://informer.minfin.com.ua/interbank/rub.js"></script>
     </div>
-    <div class="sidebar">
-        <!--LiveInternet counter-->
-        <script type="text/javascript"><!--
-            document.write( "<a href='//www.liveinternet.ru/click' " +
-                            "target=_blank><img src='//counter.yadro.ru/hit?t14.6;r" +
-                            escape( document.referrer ) + (
-                                (
-                                typeof(
-                                    screen
-                                ) == "undefined"
-                                ) ? "" :
-                                ";s" + screen.width + "*" + screen.height + "*" + (
-                                    screen.colorDepth ?
-                                        screen.colorDepth : screen.pixelDepth
-                                )
-                            ) + ";u" + escape( document.URL ) +
-                            ";" + Math.random() +
-                            "' alt='' title='LiveInternet: показано число просмотров за 24" +
-                            " часа, посетителей за 24 часа и за сегодня' " +
-                            "border='0' width='88' height='31'><\/a>" )
-            //--></script>
-        <!--/LiveInternet-->
 
-    </div>
 </div>
