001fdfb4f8d3d8e3a37fafc1409d672de0586417
Mihail Shumilov <mschumilow@gmail.com>
2015-03-22 12:53:53 +0200

    start api

---
+++ api/config/main.php
@@ -0,0 +1,43 @@
+<?php
+    $params = array_merge(
+        require( __DIR__ . '/../../common/config/params.php' ),
+        require( __DIR__ . '/../../common/config/params-local.php' ),
+        require( __DIR__ . '/params.php' ),
+        require( __DIR__ . '/params-local.php' )
+    );
+
+    return [
+        'id'                  => 'app-api',
+        'basePath'            => dirname( __DIR__ ),
+        'controllerNamespace' => 'api\controllers',
+        'bootstrap'           => [ 'log' ],
+        'modules'             => [ ],
+        'components'          => [
+            'user'         => [
+                'identityClass'   => 'common\models\User',
+                'enableAutoLogin' => true,
+            ],
+            'log'          => [
+                'traceLevel' => YII_DEBUG ? 3 : 0,
+                'targets'    => [
+                    [
+                        'class'  => 'yii\log\FileTarget',
+                        'levels' => [ 'error', 'warning' ],
+                    ],
+                ],
+            ],
+            'errorHandler' => [
+                'errorAction' => 'site/error',
+            ],
+            'urlManager'   => [
+                'enablePrettyUrl'     => true,
+                'enableStrictParsing' => false,
+                'showScriptName'      => false,
+                'rules'               => [
+                    'class' => 'yii\web\UrlRule',
+                    ''      => 'user/index',
+                ]
+            ]
+        ],
+        'params'              => $params,
+    ];
